1d21e67b48cb8151e15159f9418bb87c4cacf4e8
Digvijay Shelke <digvijaay55@gmail.com>
2026-09-12 02:35:20 +0530

    fix: use yarn berry lockfile

--- src/config/server.env.ts
+++ src/config/server.env.ts
@@ -1,9 +1,3 @@
-import { envsafe, str } from 'envsafe'
 
-export const serverEnv = envsafe({
-  RAINDROP_TOKEN: str({
-    input: process.env.RAINDROP_TOKEN,
-    allowEmpty: true,
-    default: '',
-  }),
-})
+
+
